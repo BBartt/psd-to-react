dcf2fff438dd5cc160901fa7eaef2e7148400647
bartek <bartek2@onet.eu>
2020-02-11 12:51:58 +0100

    Updates

---
+++ precache-manifest.cd8bc66a7b25db68fa12b4eb6e5d35e5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f198de31ac8f2799d688d4b4dd955b3",
+    "url": "/psd-to-react/index.html"
+  },
+  {
+    "revision": "a3d6e21ebf09407393e4",
+    "url": "/psd-to-react/static/css/2.379dd523.chunk.css"
+  },
+  {
+    "revision": "a3d6e21ebf09407393e4",
+    "url": "/psd-to-react/static/js/2.4b71e904.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/psd-to-react/static/js/2.4b71e904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba4add209583433332dd",
+    "url": "/psd-to-react/static/js/main.936e7319.chunk.js"
+  },
+  {
+    "revision": "a4ab510b3c834668bbe9",
+    "url": "/psd-to-react/static/js/runtime-main.7bf5d56b.js"
+  },
+  {
+    "revision": "72b6f2c9990bd3d37b8013a59ed78902",
+    "url": "/psd-to-react/static/media/ProximaNova-Bold.72b6f2c9.otf"
+  },
+  {
+    "revision": "517d13eb65a652f66f19d4f52eb69c6e",
+    "url": "/psd-to-react/static/media/Yrsa-Bold.517d13eb.ttf"
+  },
+  {
+    "revision": "d9d7b6a932d9ce3548e122f5d04c2d6b",
+    "url": "/psd-to-react/static/media/Yrsa-Light.d9d7b6a9.ttf"
+  },
+  {
+    "revision": "ce8a30eaa090ad47d45b8188cdb0b8bb",
+    "url": "/psd-to-react/static/media/Yrsa-Medium.ce8a30ea.ttf"
+  },
+  {
+    "revision": "7abc71357df7769d32f3093c9875ccdf",
+    "url": "/psd-to-react/static/media/Yrsa-Regular.7abc7135.ttf"
+  },
+  {
+    "revision": "69ae60a2b36be41e4a8e67ef329d8b2b",
+    "url": "/psd-to-react/static/media/Yrsa-SemiBold.69ae60a2.ttf"
+  },
+  {
+    "revision": "b58f44b8b1fede5a0c59eb28d62042c5",
+    "url": "/psd-to-react/static/media/customer-picture.b58f44b8.png"
+  },
+  {
+    "revision": "ba48025b920c36da0ade2c1a0ee0b40a",
+    "url": "/psd-to-react/static/media/page-intro-img-1.ba48025b.png"
+  },
+  {
+    "revision": "f904802d3183e3bf15b9fd0eba3b6cec",
+    "url": "/psd-to-react/static/media/page-intro-img-2.f904802d.png"
+  },
+  {
+    "revision": "f6a772e5d6a2bfdb47555f5c7c9b8682",
+    "url": "/psd-to-react/static/media/person.f6a772e5.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/psd-to-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/psd-to-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/psd-to-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/psd-to-react/static/media/slick.f97e3bbf.svg"
+  }
+]);
